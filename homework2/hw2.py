90d573c8f825b835dfa920a958ec4c4ebd3ba997
Xavier Silva <xavierasilva@berkeley.edu>
2025-02-17 20:12:44 -0800

    adding homework 2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,29 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+    # cd ../../ should work to directly go to the working directory
+
 # 2) What does ~/ mean?
+    # It means the home directory followed by a path
+
 # 3) What's the difference between and abolute and a relative path in your terminal?
+    # An absolute path starts from the root directory, showing the entire path and the relative path depends on your current location in the working directory.
+
 # 4) What command would bring you back to your home directory?
+    # cd ~ would bring you back to your home directory
+
 # 5) If you called rm ./ in your current working directory, what would it do? 
+    # It owuld not work because ./ referes to your current directory and you can not remove that
+
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+    # Git add moves files from the working directory to the staging area. Git commit saves what you add to your local repository. Git push sends your commits from yout computer to GitHub.
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+    # The message lists all files you've added with git add. It wills also give the file statuses on if they are modified, a new file, or if staged for deletion.
+
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+    #I have yet to encounter a frustrating bugs, all of them have been resolved simply so far. 
+
 # 9) How does cloning a repository differ from pulling from a repository?
+    #Cloning creates a copy of a local repository and pulling from a repository is just changing your computer from changes on a remote repository.
+
 # 10) Tell me a fun fact!
+    #Radiohead is from England
